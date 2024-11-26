5e06f53265cbdc8e3a58bc2cdcc807237d9ae61b
lizard0126 <825456471@qq.com>
2024-11-26 18:29:59 +0800

    完善

--- readme.md
+++ readme.md
@@ -2,17 +2,19 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-asmr-lizard?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-asmr-lizard)
 
-### 来一段随机的助眠asmr吧~
-
 - 获取以下类型的助眠音频：钢琴、雨声、脑波、自然
 
 - 还有睡前故事哦~
 
 - 如果是比较长的音频会分段发送~
 ---
-## 如果喜欢我的插件
 <details>
+<summary>如果要反馈建议或报告问题</summary>
 
-可以[请我喝可乐](https://ifdian.net/a/lizard0126)，没准就有动力更新新功能了
+可以[点这里](https://github.com/lizard0126/asmr-lizard/issues)创建议题~
+</details>
+<details>
+<summary>如果喜欢我的插件</summary>
 
+可以[请我喝可乐](https://ifdian.net/a/lizard0126)，没准就有动力更新新功能了~
 </details>
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import { Context, Schema, h } from 'koishi';
 // npm publish --workspace koishi-plugin-asmr-lizard --access public --registry https://registry.npmjs.org
 import { } from 'koishi-plugin-ffmpeg';
-
 export const name = 'asmr-lizard';
 export const inject = ['ffmpeg'];
-
 export interface Config { }
-
 export const Config = Schema.object({});
-
 export const usage = `
 - 获取以下类型的助眠音频：钢琴、雨声、脑波、自然
 
@@ -16,11 +12,15 @@ export const usage = `
 
 - 如果是比较长的音频会分段发送~
 ---
-## 如果喜欢我的插件
 <details>
+<summary>如果要反馈建议或报告问题</summary>
 
-可以[请我喝可乐](https://ifdian.net/a/lizard0126)，没准就有动力更新新功能了
+可以[点这里](https://github.com/lizard0126/asmr-lizard/issues)创建议题~
+</details>
+<details>
+<summary>如果喜欢我的插件</summary>
 
+可以[请我喝可乐](https://ifdian.net/a/lizard0126)，没准就有动力更新新功能了~
 </details>
 `;
 
@@ -36,7 +36,7 @@ export function apply(ctx: Context) {
   ctx.command('助眠 [type]', '获取助眠音频，包括小故事')
     .action(async ({ session }, type) => {
       if (!type) {
-        return '提供以下类型：钢琴、雨声、脑波、自然、故事，或输入随机。';
+        return '提供以下类型：钢琴、雨声、脑波、自然、故事，输入随机则随机获取。';
       }
 
       if (type === '随机') {
@@ -45,20 +45,17 @@ export function apply(ctx: Context) {
       }
 
       const apiUrl = apis[type];
-      // 检查 apiUrl 是否存在
       if (!apiUrl) {
-        return '提供以下类型：钢琴、雨声、脑波、自然、故事，或输入随机。';
+        return '提供以下类型：钢琴、雨声、脑波、自然、故事，输入随机则随机获取。';
       }
 
       try {
         const response = await ctx.http.get(apiUrl);
         const { title, cover, url } = response;
 
-        // 获取音频数据
         const audioData = await ctx.http.get<ArrayBuffer>(url, { responseType: 'arraybuffer' });
         const buffer = Buffer.from(audioData);
 
-        // 使用 FFmpeg 获取音频时长
         const durationBuffer = await ctx.ffmpeg.builder()
           .input(buffer)
           .outputOption('-f', 'null')
@@ -68,14 +65,14 @@ export function apply(ctx: Context) {
         const match = /Duration: (\d+):(\d+):(\d+\.\d+)/.exec(durationOutput);
         const duration = match ? (parseInt(match[1]) * 3600 + parseInt(match[2]) * 60 + parseFloat(match[3])) : 0;
 
-        // 获取并发送封面图片
         const coverResponse = await ctx.http.get(cover);
         await session.send(`${title}\n${h.image(coverResponse)}`);
 
-        const maxSegmentDuration = 5 * 60; // 最大段时长为5分钟
+        const maxSegmentDuration = 5 * 60;
+        const [tipMessageId] = await session.send('正在获取音频，请耐心等待');
         if (duration <= maxSegmentDuration) {
-          // 对于时长小于5分钟的音频，直接发送 MP3 格式
           await session.send(h.audio(buffer, 'audio/mp3'));
+          await session.bot.deleteMessage(session.channelId, tipMessageId);
         } else {
           const segmentCount = Math.ceil(duration / maxSegmentDuration);
           await session.send(`音频时长大于5分钟，将分${segmentCount}段发送。`);
@@ -83,23 +80,20 @@ export function apply(ctx: Context) {
           for (let i = 0; i < segmentCount; i++) {
             const startTime = i * maxSegmentDuration;
 
-            // 提取并转换每段为 MP3 格式
             const segmentBuffer = await ctx.ffmpeg.builder()
               .input(buffer)
               .inputOption('-ss', startTime.toString())
               .outputOption('-t', maxSegmentDuration.toString(), '-f', 'mp3')
               .run('buffer');
 
-            // 添加重试机制
             const maxRetries = 3; // 最大重试次数
             let attempts = 0;
             let sent = false;
 
             while (attempts < maxRetries && !sent) {
               try {
-                // 发送每段音频
                 await session.send(h.audio(segmentBuffer, 'audio/mp3'));
-                sent = true; // 成功发送，退出循环
+                sent = true;
               } catch (error) {
                 attempts++;
                 await session.send(`发送第 ${i + 1} 段音频失败，正在重试...（尝试次数: ${attempts}）`);
@@ -109,6 +103,7 @@ export function apply(ctx: Context) {
               }
             }
           }
+          await session.bot.deleteMessage(session.channelId, tipMessageId);
         }
       } catch (error) {
         ctx.logger.error('请求失败', error.message);
